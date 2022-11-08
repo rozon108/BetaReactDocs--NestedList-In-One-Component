88bb46f83c78878e21c44d233b8577b11253a915
Rojan <rojan108p@gmail.com>
2022-11-08 17:05:26 +1100

    tried

--- src/App.js
+++ src/App.js
@@ -6,9 +6,24 @@ import { recipes } from "./data.js";
 // list its ingredients in a <ul>.
 
 export default function RecipeList() {
+  const recipeList = recipes.map((item) => {
+    return (
+      <div key={item.id}>
+        <h2>{item.name}</h2>
+        <ul>
+          {/* List component
+      <RecipeIngredients */}
+          <li>list of ingredients</li>
+          <li>{item.ingredients}</li>
+        </ul>
+      </div>
+    );
+  });
+
   return (
     <div>
       <h1>Recipes</h1>
+      {recipeList}
     </div>
   );
 }
