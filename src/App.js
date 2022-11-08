1e250a3c4aa02bf025aa7286ea0c376a59702796
Rojan <rojan108p@gmail.com>
2022-11-08 16:50:16 +1100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { recipes } from "./data.js";
+
+// Make a list of recipes from this array!
+// For each recipe in the array,
+// display its title as an <h2> and
+// list its ingredients in a <ul>.
+
+export default function RecipeList() {
+  return (
+    <div>
+      <h1>Recipes</h1>
+    </div>
+  );
+}
