80d3eafed4097b61a5b9b48b5560317058f6f706
Rojan <rojan108p@gmail.com>
2022-11-08 23:31:33 +1100

    Seperated html list/div into RecipeComponent use of props and passing value/objects using .map and "..."
    solution from :
    https://beta.reactjs.org/learn/rendering-lists

--- src/App.js
+++ src/App.js
@@ -4,23 +4,28 @@ import { recipes } from "./data.js";
 // For each recipe in the array,
 // display its title as an <h2> and
 // list its ingredients in a <ul>.
+function RecipeComponent({ id, name, ingredients }) {
+  return (
+    <div>
+      <h2>{name}</h2>
+
+      <ul>
+        {/* List component
+<RecipeIngredients */}
+        {ingredients.map((x) => (
+          <li>{x}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
 
 export default function RecipeList() {
   return (
     <div>
       <h1>Recipes</h1>
       {recipes.map((item) => (
-        <div key={item.id}>
-          <h2>{item.name}</h2>
-
-          <ul>
-            {/* List component
-      <RecipeIngredients */}
-            {item.ingredients.map((x) => (
-              <li>{x}</li>
-            ))}
-          </ul>
-        </div>
+        <RecipeComponent {...item} key={item.id} />
       ))}
     </div>
   );
